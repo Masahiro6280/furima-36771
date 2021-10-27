d26a4ef4e0b517183b88bb90c17ed1a9c6765b07
Masahiro6280 <purple08260826@gmail.com>
2021-10-27 19:39:28 +0900

    ユーザー認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,20 @@
 class ApplicationController < ActionController::Base
  
  
- 
- 
- 
- 
-  # before_action :basic_auth
-  # private
-
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'admin' && password == '2222'
-  #   end
-  # end
-  # basic認証についての記述
-
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-  #  before_action :authenticate_user!
+ 
+  before_action :basic_auth
+
+
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_huri, :last_name_huri, :birthday])
   end
 end
-
+end
